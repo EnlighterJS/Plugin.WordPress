f70599968dac57c20276d09108fb6e713488911e
Andi Dittrich <andi@andidittrich.de>
2020-04-15 17:12:49 +0200

    removed webfonts loader

--- modules/core/FontManager.php
+++ modules/core/FontManager.php
@@ -38,25 +38,4 @@ class FontManager{
             return array_values($fontlist)[0];
         }
     }
-    
-    function enqueue(){
-        // list of google webfonts to load
-        $webfontList = array();
-
-        // get all available webfonts
-        $webfonts = GoogleWebfontResources::getMonospaceFonts();
-
-        // load enabled fonts
-        foreach ($webfonts as $name => $font){
-            $fid = preg_replace('/[^A-Za-z0-9]/', '', $name);
-            if ($this->_config['webfonts'.$fid]){
-                $webfontList[] = $font;
-            }
-        }
-
-        // load webfonts ?
-        if (count($webfontList) > 0){
-            $this->enqueueStyle('enlighter-webfonts', '//fonts.googleapis.com/css?family=' . implode('|', $webfontList));
-        }
-    }
 }
